b8f4947b335a0b8be3594e996f55c72b0a29f1b1
rob tillaart <rob.tillaart@gmail.com>
2021-01-17 17:27:22 +0100

    unit test

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -45,22 +45,10 @@ unittest_teardown()
 {
 }
 
-/*
-unittest(test_new_operator)
-{
-  assertEqualINF(exp(800));
-  assertEqualINF(0.0/0.0);
-  assertEqualINF(42);
-  
-  assertEqualNAN(INFINITY - INFINITY);
-  assertEqualNAN(0.0/0.0);
-  assertEqualNAN(42);
-}
-*/
 
 unittest(test_constructor)
 {
-  fprintf(stderr, "VERSION: %s\n", );
+  fprintf(stderr, "VERSION: %s\n", MULTIPLEX_LIB_VERSION);
   
   assertEqual(1, 1);
 }
